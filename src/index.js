1497f84bfccd1fc6d84729b0ec08c57f07fa96a6
sillyU-jpg <nikolaiking7@gmail.com>
2025-01-15 19:02:25 +1300

    added firebase server connection through webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvYf2-mdKC_TsoI7fpoVK5B0C4CeRJuis",
+    authDomain: "imagediarynk.firebaseapp.com",
+    projectId: "imagediarynk",
+    storageBucket: "imagediarynk.firebasestorage.app",
+    messagingSenderId: "185040970597",
+    appId: "1:185040970597:web:fc191de4a14ceec828f78c",
+    measurementId: "G-1JW7YWPW2F"
+  };
